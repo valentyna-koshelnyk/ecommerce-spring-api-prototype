b422dbe2e5274ac81e6d765f124991828549ea22
Valentyna Koshelnyk <vakoshelnik@gmail.com>
2024-01-12 17:45:45 +0100

    [COM-25] Simplify to ROLE_USER and ROLE_ADMIN

--- src/main/java/com/startsteps/ecommerceapi/user/model/UserRoles.java
+++ src/main/java/com/startsteps/ecommerceapi/user/model/UserRoles.java
@@ -1,5 +1,5 @@
 package com.startsteps.ecommerceapi.user.model;
 
 public enum UserRoles {
-    ROLE_CUSTOMER, ROLE_SELLER, ROLE_ADMIN
+    ROLE_USER, ROLE_ADMIN
 }
---
+++ src/main/java/com/startsteps/ecommerceapi/user/service/EcomUserAdapter.java
@@ -0,0 +1,50 @@
+package com.startsteps.ecommerceapi.user.service;
+import com.startsteps.ecommerceapi.user.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+public class EcomUserAdapter implements UserDetails {
+    private final User user;
+
+    public EcomUserAdapter(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getAuthority()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
