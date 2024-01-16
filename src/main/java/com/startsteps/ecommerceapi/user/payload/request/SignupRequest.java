9dcaa544213cec381c6fa84db14af362c6a367e9
Valentyna Koshelnyk <vakoshelnik@gmail.com>
2024-01-16 16:58:38 +0100

    [COM-13] add classes which will handle data encapsulation from login/sign up form (DTO between service and controller)

---
+++ src/main/java/com/startsteps/ecommerceapi/user/payload/request/SignupRequest.java
@@ -0,0 +1,37 @@
+package com.startsteps.ecommerceapi.user.payload.request;
+
+import com.startsteps.ecommerceapi.user.validation.ValidEmail;
+import com.startsteps.ecommerceapi.user.validation.ValidPassword;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+@Getter
+@Setter
+public class SignupRequest {
+    private Long userId;
+
+    @NotNull(message = "Username cannot be null")
+    @NotEmpty(message = "Username cannot be empty")
+    private String username;
+
+    @ValidEmail(message = "Email is not valid")
+    @NotNull(message = "Email cannot be null")
+    @NotEmpty(message = "Email cannot be empty")
+    private String email;
+
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @ValidPassword
+    private String password;
+
+    @NotNull(message = "Matching password cannot be null")
+    @NotEmpty(message = "Matching password cannot be empty")
+    private String matchingPassword;
+
+    private Set<String> role;
+}
+  
+
---
+++ src/main/java/com/startsteps/ecommerceapi/user/payload/response/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.startsteps.ecommerceapi.user.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MessageResponse {
+	private String message;
+
+}
