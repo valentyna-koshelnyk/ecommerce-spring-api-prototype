3f17fa83a0975e7a77e9ba48cd060aec9e92bce9
Valentyna Koshelnyk <vakoshelnik@gmail.com>
2024-02-21 10:24:07 +0100

    [COM 104] add prod properties, deploy the app on google cloud run (https://ecomapp-laowthm57a-oe.a.run.app/)

--- src/main/java/com/startsteps/ecommerceapi/persistence/UserRepository.java
+++ src/main/java/com/startsteps/ecommerceapi/persistence/UserRepository.java
@@ -2,9 +2,7 @@ package com.startsteps.ecommerceapi.persistence;
 
 import com.startsteps.ecommerceapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
